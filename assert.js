547e0d0822520619d12090dd70808460583f20af
Bret Cameron <bretcameron@gmail.com>
2019-08-14 23:53:31 +0100

    Collect Mocha test data for manipulation using Mocha API

--- assert.js
+++ assert.js
@@ -1,16 +1,22 @@
 const assert = require('assert');
-describe('top suite', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-    it('should return 2 when the value 3 is present', function () {
-      assert.equal([1, 2, 3].indexOf(3), 2);
+describe('TEST TITLE 1', function () {
+  describe('TEST TITLE 2', function () {
+    describe('TEST TITLE 3', function () {
+      it('should return -1 when the value is not present', function () {
+        assert.equal([1, 2, 3].indexOf(4), -1);
+      });
+      it('should return 2 when the value 3 is present', function () {
+        assert.equal([1, 2, 3].indexOf(2), 1);
+      });
     });
   });
-  describe('yoyo', function () {
-    it('should return 1 when the value 2 is present', function () {
-      assert.equal([1, 2, 3].indexOf(2), 1);
-    });
+});
+
+describe('TEST TITLE 4', function () {
+  it('should return -1 when the value is not present', function () {
+    assert.equal([1, 2, 3].indexOf(4), -1);
+  });
+  it(`should return ${[1, 2, 3].indexOf(2)} when the value 2 is present`, function () {
+    assert.equal([1, 2, 3].indexOf(2), 1);
   });
 });
--- test/test.js
+++ test/test.js
@@ -1,42 +1,78 @@
 const Mocha = require('mocha');
+const util = require('util');
+const test = new Mocha({ ui: 'bdd' });
 
-const test = new Mocha();
 test.addFile('./assert.js');
-let runner = test.run();
 
-const passed = [];
-const failed = [];
+const runner = test.run();
 
 const runTests = async () => {
-  const tests = [];
-
   return new Promise((resolve, reject) => {
+    const arr = [];
+    let obj = {};
+    const data = {};
+    let currentSuite = '';
+
+    // runner.on('suite end', (e) => {
+    // });
 
     runner.on('suite', (e) => {
-      tests.push({
-        suite: e.title,
-      });
+      if (obj.describe) {
+        arr.push(obj);
+        obj = {};
+      }
+
+      const { title, parent } = e;
+
+      let parentName = parent.title;
+      let nextParent = e.parent, nextTitle, count = 0;
+
+      while (nextTitle !== '') {
+        try {
+          nextParent = nextParent.parent;
+          nextTitle = nextParent.title;
+          count++;
+        } catch {
+          break;
+        }
+      }
+
+      // Set current suite based on title
+      currentSuite = title;
+
+      // Styling aid, suggesting how much to indent by
+      obj._indent = count;
+      if (title) obj.describe = title;
+      if (parent.title) obj.parent = parent.title;
+
     });
 
-    runner.on('pass', (e) => {
-      tests.push({
-        title: e.title,
-        state: e.state,
-      });
-    });
+    runner.on('test end', (e) => {
+      const { title, state } = e;
 
-    runner.on('fail', (e) => {
-      tests.push({
-        title: e.title,
-        state: e.state
-      });
+      // If there are any tests, push their title and state ('passed' or 'failed')
+      if (title && state) {
+        if (!obj.tests) obj.tests = [];
+        obj.tests.push({
+          it: title,
+          passed: state === 'passed',
+        });
+      };
     });
 
     runner.on('end', (e) => {
-      resolve(tests);
+      if (obj.describe) {
+        arr.push(obj);
+        obj = {};
+      }
+      resolve(arr);
     });
-  })
+  });
 };
 
 runTests()
-  .then(e => console.log(e))
+  .then(data => console.log(
+    util.inspect(data, {
+      showHidden: false, depth: 4
+    })
+  ));
